65eff6e0d301517af87218d562312ef1726014ab
Miguel <forgetmiguel@hotmail.com>
2022-05-02 00:50:27 +0200

    Add track order page

---
+++ FunTrophy.Web/Pages/Editor/TrackOrder/Index.razor.cs
@@ -0,0 +1,15 @@
+﻿using FunTrophy.Shared.Model;
+
+namespace FunTrophy.Web.Pages.Editor.TrackOrder
+{
+    public partial class Index
+    {
+        public List<ColorDto> Colors { get; set; }
+        public List<TrackDto> Tracks { get; set; }
+        public Index()
+        {
+            Colors = FakeModel.Colors;
+            Tracks = FakeModel.Tracks;
+        }
+    }
+}
