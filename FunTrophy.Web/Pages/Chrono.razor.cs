6836d875101adbc0752ce6da05d429571b8593a4
Miguel <forgetmiguel@hotmail.com>
2022-05-26 16:01:32 +0200

    Add ColorSelector component

---
+++ FunTrophy.Web/Pages/Chrono.razor.cs
@@ -0,0 +1,33 @@
+﻿using FunTrophy.Shared.Model;
+using FunTrophy.Web.Contracts.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace FunTrophy.Web.Pages
+{
+    public partial class Chrono
+    {
+        #region Properties
+        [Inject]
+        private AppState AppState { get; set; } = default!;
+
+        [Inject]
+        private IColorService ColorService { get; set; } = default!;
+
+        private List<ColorDto>? Colors { get; set; }
+
+        #endregion
+
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadColors();
+        }
+
+        public async Task LoadColors()
+        {
+            if (AppState.Race != null)
+            {
+                Colors = await ColorService.GetColors(AppState.Race.Id);
+            }
+        }
+    }
+}
