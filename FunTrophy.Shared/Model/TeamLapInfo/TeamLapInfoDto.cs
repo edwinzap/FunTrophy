a0d030c8849a6fb52346f93c572be1191562bc70
Miguel <forgetmiguel@hotmail.com>
2022-05-26 17:49:17 +0200

    Create chrono page

---
+++ FunTrophy.Shared/Model/TeamLapInfo/TeamLapInfoDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunTrophy.Shared.Model
+{
+    public class TeamLapInfoDto
+    {
+        public TeamDto Team { get; set; }
+        public TrackDto? CurrentTrack { get; set; }
+
+        public TrackDto? NextTrack { get; set; }
+        public DateTime? CurrentTrackStartTime { get; set; }
+    }
+}
--- FunTrophy.Web/Pages/Chrono.razor.cs
+++ FunTrophy.Web/Pages/Chrono.razor.cs
@@ -15,11 +15,33 @@ namespace FunTrophy.Web.Pages
 
         private List<ColorDto>? Colors { get; set; }
 
+        private List<TeamLapInfoDto>? Laps { get; set; }
+
+        private DateTime CurrentDateTime { get; set; } = DateTime.Now;
+
+        public int? CurrentColorId { get; set; }
+
+        private Timer? timer;
+
         #endregion
 
         protected override async Task OnInitializedAsync()
         {
             await LoadColors();
+            await LoadLaps();
+            StartTime();
+        }
+
+        private void StartTime()
+        {
+            timer = new Timer(_ =>
+            {
+                if (Laps?.Any() == true)
+                {
+                    CurrentDateTime = DateTime.Now;
+                }
+                StateHasChanged();
+            }, new AutoResetEvent(false), 1000, 1000); // fire every 2000 milliseconds
         }
 
         public async Task LoadColors()
@@ -28,6 +50,61 @@ namespace FunTrophy.Web.Pages
             {
                 Colors = await ColorService.GetColors(AppState.Race.Id);
             }
+        }
+
+        public async Task LoadLaps()
+        {
+            Laps = new List<TeamLapInfoDto>
+            {
+                new TeamLapInfoDto
+                {
+                    CurrentTrack = null,
+                    CurrentTrackStartTime = null,
+                    Team = new TeamDto
+                    {
+                        Id = 1,
+                        Number = 1,
+                        Name = "Mon équipe 1",
+                    },
+                    NextTrack = new TrackDto
+                    {
+                        Id = 1,
+                        Name = "Le Parcours 1"
+                    }
+                },
+                new TeamLapInfoDto
+                {
+                    CurrentTrack = new TrackDto
+                    {
+                        Id = 1,
+                        Name = "Le Parcours 1"
+                    },
+                    CurrentTrackStartTime = DateTime.Now,
+                    Team = new TeamDto
+                    {
+                        Id = 2,
+                        Number = 2,
+                        Name = "Mon équipe 2",
+                    },
+                    NextTrack = new TrackDto
+                    {
+                        Id = 1,
+                        Name = "Le Parcours 2"
+                    }
+                }
+            };
+            await Task.CompletedTask;
+        }
+
+        private async Task OnCurrentColorChanged(int colorId)
+        {
+            CurrentColorId = colorId;
+            await LoadLaps();
+        }
+
+        private async Task OnStopClicked(int teamId)
+        {
+
         }
     }
 }
