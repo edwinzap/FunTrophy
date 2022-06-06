53e1c8fdd20ce2d9dc81186c37d4af1fc25cbf9b
Miguel Forget <Miguel.Forget@rodekruis.be>
2022-06-06 14:13:50 +0200

    Add SignalR server side

--- FunTrophy.API/Program.cs
+++ FunTrophy.API/Program.cs
@@ -27,6 +27,7 @@ builder.Services
     //.AddFakeRepositories();
     .AddRepositories();
 
+builder.Services.AddSignalR();
 builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -54,5 +55,5 @@ app.UseCors(corsPolicyName);
 app.UseAuthorization();
 
 app.MapControllers();
-
+//app.MapHub<>();
 app.Run();
---
+++ FunTrophy.Shared/HubConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace FunTrophy.Shared
+{
+    public static class HubConstants
+    {
+        public static readonly string TrackTimeChanged = "TrackTimeChanged";
+        public static readonly string TimeAdjustmentChanged = "TimeAdjustmentChanged";
+    }
+}
