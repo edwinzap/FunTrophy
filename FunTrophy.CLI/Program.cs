03a12fe617f8519b844bcdedc9f739413817a00a
Miguel <forgetmiguel@hotmail.com>
2023-04-29 10:07:00 +0200

    Add sqllite

--- FunTrophy.CLI/Program.cs
+++ FunTrophy.CLI/Program.cs
@@ -1,26 +1,15 @@
-﻿using FunTrophy.API;
-using FunTrophy.API.Contracts.Services;
-using FunTrophy.Fake;
+﻿using FunTrophy.Fake;
 using FunTrophy.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 Console.WriteLine("Hello, World!");
 
-//var connectionString = "Server=127.0.0.1,1433;Initial Catalog=FunTrophy;User ID=SA;Password=Admin123!;Integrated Security=False;MultipleActiveResultSets=False;TrustServerCertificate=True;Connection Timeout=30;";
-var connectionString = "Server=(LocalDb)\\MSSQLLocalDB;Initial Catalog=funtrophy;Integrated Security=True;MultipleActiveResultSets=False;";
+var connectionString = "Data Source=C:/Users/forge/source/repos/FunTrophy/FunTrophy.Infrastructure/FunTrophy.sqlite";
 var services = new ServiceCollection();
 
-//services
-//    .AddServices()
-//    .AddMappers()
-//    .AddHelpers()
-//    .AddRepositories()
-//    .AddSignalR();
-
-//services.AddLogging();
 services.AddDbContext<FunTrophyContext>(options =>
-        options.UseSqlServer(connectionString), ServiceLifetime.Transient)
+        options.UseSqlite(connectionString), ServiceLifetime.Transient)
     .AddTransient<FunTrophyInitializer>();
 
 var serviceProvider = services
