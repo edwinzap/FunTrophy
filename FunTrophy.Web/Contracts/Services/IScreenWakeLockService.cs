396de8ee1026bf6d7d5744e644f54ccfe6562af3
edwinzap <tictactiptop@hotmail.com>
2024-06-16 16:32:38 +0200

    Add screen wake lock on the Chrono page

---
+++ FunTrophy.Web/Contracts/Services/IScreenWakeLockService.cs
@@ -0,0 +1,17 @@
+﻿using FunTrophy.Web.Models;
+
+namespace FunTrophy.Web.Contracts.Services
+{
+    public interface IScreenWakeLockService
+    {
+        // Requests a screen wake lock and returns a sentinel object
+        Task<WakeLockSentinel> RequestWakeLockAsync();
+
+        // Releases a screen wake lock given a sentinel object
+        Task ReleaseWakeLockAsync(WakeLockSentinel sentinel);
+
+        // Checks if the browser supports the screen wake lock API
+        Task<bool> IsSupportedAsync();
+    }
+
+}
